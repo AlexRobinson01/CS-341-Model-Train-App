58bc2cf18aee73154693a7a2da8721d911dcfe7d
andrewmartinx <martia42@uwosh.edu>
2024-10-06 17:46:27 -0500

    Added project, icons, login page

---
+++ ModelTrain/ModelTrain/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ModelTrain
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+            // Navigate to the login page when the app starts
+            Shell.Current.GoToAsync("//login");
+        }
+    }
+}
---
+++ ModelTrain/ModelTrain/Screens/Login.xaml.cs
@@ -0,0 +1,14 @@
+namespace ModelTrain.Screens;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+
+    private async void OnLoginButtonClicked(object sender, EventArgs e)
+    {
+        // Login logic
+    }
+}
