91fd3c293cf9779ec5984f2402c8720bfa4e99d2
andrewmartinx <martia42@uwosh.edu>
2024-10-22 20:37:53 -0500

    changed back to start on login screen and added navigation from personal projects to edit and configure project screens

--- ModelTrain/ModelTrain/App.xaml.cs
+++ ModelTrain/ModelTrain/App.xaml.cs
@@ -8,7 +8,7 @@ namespace ModelTrain
             InitializeComponent();
 
             //CHANGE new NavigationPage(new YOURSCREEN()) to view screen
-            MainPage = new NavigationPage(new Account());
+            MainPage = new NavigationPage(new Login());
 
             //MainPage = new AppShell();
             //// Navigate to the login page when the app starts
