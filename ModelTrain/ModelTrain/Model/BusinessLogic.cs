1748bfadb734491631ef13b194d86303635b08ae
andrewmartinx <martia42@uwosh.edu>
2024-12-05 15:19:27 -0600

    Added comments to BusinessLogic layer

--- ModelTrain/ModelTrain/Model/BusinessLogic.cs
+++ ModelTrain/ModelTrain/Model/BusinessLogic.cs
@@ -10,6 +10,7 @@ namespace ModelTrain.Model
         private static BusinessLogic _instance;
         // Lock object for thread safety
         private static readonly object _lock = new object();
+        // This variable will hold the signed in users email upon login
         private String email = "";
 
         public BusinessLogic()
@@ -17,7 +18,7 @@ namespace ModelTrain.Model
             Database = new Database();
         }
 
-        // Public method to get the single instance of BusinessLogic
+        // Public method to get the single instance of BusinessLogic (allows for locally stored email upon login)
         public static BusinessLogic Instance
         {
             get
@@ -39,8 +40,10 @@ namespace ModelTrain.Model
 
         // Get user with email
         public User GetUserFromEmail()
-        {
+        {   
+            //Grab the user from their email
             User userToGet = Database.GetUser(this.email);
+            // If user exists, return them
             if (userToGet != null)
             {
                 return userToGet;
@@ -51,12 +54,14 @@ namespace ModelTrain.Model
             }
         }
 
+        // Save changes made to a project
         public bool SaveProject(PersonalProject project)
         {
             // TODO: save project to database; allow for shared projects too
             return new Random().Next() % 2 == 0;
         }
 
+        // Make sure login input exists in the database and is correct
         public async Task<bool> ValidateLoginInput(String email, String password)
         {
             // Check if email is not in database
@@ -68,10 +73,12 @@ namespace ModelTrain.Model
             {
                 return false;
             }
+            // Locally store email of signed in user for other functionality
             this.email = email;
             return true;
         }
 
+        // Create a new user account
         public async Task<bool> CreateAccount(String firstName, String lastName, String email, String password)
         {
             bool uniqueEmail = await Database.IsNewEmail(email);        // Check if database doesn't have this email
@@ -84,8 +91,10 @@ namespace ModelTrain.Model
             
         }
 
+        // Takes a project Id and deletes the project from the users profile and the projects db table
         public async Task<bool> DeleteProjectById(String projectId)
         {
+            // If project is deleted from both tables, return true
             if (await Database.DeletePersonalProject(projectId) && await Database.RemoveProjectFromUsersAsync(projectId))
             {
                 return true;
@@ -93,28 +102,40 @@ namespace ModelTrain.Model
             return false;
         }
 
+        // Fetches a list of user projects ids
         public async Task<List<Guid>> GetUserProjects()
         {
+            // Create a new list of Guids to hold the users project ids
             List<Guid> userProjects = new List<Guid>();
+            // Fetch list from the db and return
             userProjects = await Database.GetUserProjectIdsAsync(this.email);
             return userProjects;
         }
 
+        // Fetches a list of user projects
         public async Task<List<PersonalProject>> GetProjectsByIds(List<Guid> projectIds)
         {
+            // Create a new list of personal projects
             List<PersonalProject> userProjects = new List<PersonalProject>();
+            // Fetch list of personal projects from the db and return
             userProjects = await Database.GetProjectsByIdsAsync(projectIds);
             return userProjects;
         }
 
+        // Creates a unique Guid for a new project
         public async Task<string> GetUniqueGuid(int maxRetries = 10)
         {
+            // Create new Guid
             Guid newGuid = Guid.NewGuid();
+            // Set try count to zero
             int retryCount = 0;
 
+            // If projectid is unique in the database, return
             while (!(await Database.IsGuidUnique(newGuid)))
             {
+                // If not unique, try again and increment count
                 retryCount++;
+                // If reaches max attempts, relay that it cannot generate unique GUID
                 if (retryCount > maxRetries)
                 {
                     throw new InvalidOperationException("Failed to generate a unique GUID after multiple attempts.");
@@ -126,8 +147,10 @@ namespace ModelTrain.Model
             return newGuid.ToString();
         }
 
+        // Adds a new project to the users profile and the projects db table
         public async Task<bool> AddProjectToDB(PersonalProject newProject)
         {
+            // Add new project to both db tables, if no errors, return true
             if (await Database.AddProjectToProjects(this.email, newProject) && await Database.AddProjectToUser(this.email, newProject.ProjectID))
             {
                 return true;
