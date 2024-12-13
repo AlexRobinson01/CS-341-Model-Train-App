059ede4e9c2afb4da44c79f7e45bd9b1083c88c4
AlexRobinson01 <131490805+AlexRobinson01@users.noreply.github.com>
2024-12-13 17:12:47 -0600

    Fixed new font not being configured for the app

--- ModelTrain/ModelTrain/MauiProgram.cs
+++ ModelTrain/ModelTrain/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace ModelTrain
                 .UseSkiaSharp() // The savior for touch events!
                 .ConfigureFonts(fonts =>
                 {
+                    fonts.AddFont("PTSerif-Regular.ttf", "PTSerifRegular");
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                     fonts.AddFont("MaterialIcons-Regular.ttf", "GoogleMaterialFont");
